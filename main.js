9e5f89cce6c85f9d53a2a04e503fecc1d979f66e
aalliina <90504909+aalliina@users.noreply.github.com>
2021-12-18 14:46:03 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,44 @@
+// name1 = prompt('Введите ваше имя');
+// console.log(name1)
+// alert(`Ваше имя: ${name1}` );
+// age = prompt('Введите ваш возраст');
+// console.log(age)
+// alert(`Ваш возраст: ${age}` );
+// if (age >=1 && age <=3){
+//     alert(`Ранний возраст`)
+// }
+// if (age >=4 && age <=6){
+//     alert(`Дошкольный возраст`)
+// }
+// if (age >=7 && age <=13){
+//     alert(`Школьный возраст`)
+// }
+// if (age >=14 && age <=17){
+//     alert(`Подростковый возраст`)
+// }
+// if(age >= 18){
+//     alert("Взрослый")
+// }
+
+
+
+// const year = 2021
+// user_age = prompt('Введите год вашего рождения');
+// console.log(user_age)
+// alert(year - user_age);
+
+
+// age = prompt('Какая длина стороны квадрата');
+// console.log(age)
+// alert(`Сторона квадрата: ${age}`);
+// alert(`Периметр квадрата: ${age * 4}`);
+
+
+
+// grn = prompt('Сколько у вас гривен');
+// console.log(grn)
+// const euro = 0.033
+// const dollar = 0.036
+// alert(`Евро: ${grn * euro}`)
+// alert(`Долларов: ${grn * dollar}`)
+
